dc0c28850ab586328bb00f572c9b79e8a5dcea18
Maxim Farnworth <maximfarnworth@outlook.com>
2024-12-12 12:31:50 +0000

    Update Program.cs

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -3,48 +3,44 @@ namespace Day7
     
     internal class Program
     {
-        static bool AddOrTimes(int[] factors, int total, int index, int sum)//add two numbers together or times two numbers and keeps track of whats been added and whats been timesd
+        static bool AddOrTimes(ulong[] factors, ulong total, int index, ulong sum)
         {
-            int numberOfOperators = factors.Length - 1;
-            index++;
-            sum += factors[index];
-            if (sum == total&&index!=numberOfOperators)
+            //add two numbers together or times two numbers and keeps track of whats been added and whats been timesd
+            if (index == factors.Length)
             {
-                return true;
+                return sum == total;
             }
-            else if (index==numberOfOperators)
+            if (sum >= total && index != factors.Length)
             {
                 return false;
             }
-            else
-            {
-                AddOrTimes(factors, total, index, sum);
-            }
-            return false;
-            
-        }
 
+            return AddOrTimes(factors, total, index + 1, sum + factors[index])
+                || AddOrTimes(factors, total, index + 1, sum * factors[index]);
+        }
 
         static void Main()
         {
             string[] input = File.ReadAllLines("input.txt");//dotnetperls.com/category
-            int answer = 0;//how many are true
-            int[] factors;
+            ulong answer = 0;//how many are true
+            ulong[] factors;
             foreach (string line in input)
             {
-                int total = 0;
+                ulong total = 0;
                 string[] bothNumbers = line.Split(':');
                 string[] factorString = bothNumbers[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                factors =(from value in factorString
-                                select int.Parse(value)).ToArray();
-                total = int.Parse(bothNumbers[0]);
-                if (AddOrTimes(factors, total, 0,0))
+                factors = (from value in factorString
+                                select ulong.Parse(value)).ToArray();
+                total = ulong.Parse(bothNumbers[0]);
+                if (AddOrTimes(factors, total, 1, factors[0]))
                 {
-                    answer++;
+                    answer += total;
                 }
-                Console.WriteLine(answer);
-               
             }
+            Console.WriteLine(answer);
+            //32033638746 too low
+            //1297330561377 too low
+
         }
     }
 }
